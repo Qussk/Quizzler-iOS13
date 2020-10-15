ba269ec19907ebb9d2bb5ac4983e4461c5c67022
Qussk <by1106007@gmail.com>
2020-10-15 18:26:14 +0900

    feat: oulet,setUIdata

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  //아울렛
-  
-  
-  
+  //스코어,질문,진행률바
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var QuestionLabel: UILabel!
+  @IBOutlet weak var ProgressView: UIProgressView!
   
   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      setData()
     }
-
+  
+  
+  //2.값 설정
+  func setData(){
+    self.scoreLabel.text = "score = 0"
+    self.QuestionLabel.text = "1"
+    self.ProgressView.progress = 0.0
+  }
+  
+  //action
+  @IBAction func buttonDidTap(_ sender: UIButton) {
+    
+  }
+  
 
 }
 
